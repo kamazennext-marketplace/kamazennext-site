95b249db0208cf56054317e72388d18f88ce09fc
kamazennext-marketplace <kamazennext@gmail.com>
2026-01-25 22:51:05 +0530

    Bypass loader rewrite for marketplace + add health checks

--- public_html/kzn-health.php
+++ public_html/kzn-health.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-echo "MARKETPLACE PHP OK\n";
+echo "KZN ROOT PHP OK\n";
 echo "uri=" . ($_SERVER['REQUEST_URI'] ?? '') . "\n";
 echo "time=" . date('c') . "\n";
 ?>
--- public_html/software/software/marketplace/health.php
+++ public_html/software/software/marketplace/health.php
@@ -2,6 +2,5 @@
 header('Content-Type: text/plain; charset=utf-8');
 echo "MARKETPLACE PHP OK\n";
 echo "uri=" . ($_SERVER['REQUEST_URI'] ?? '') . "\n";
-echo "script=" . (__FILE__) . "\n";
 echo "time=" . date('c') . "\n";
 ?>
